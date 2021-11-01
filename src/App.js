b0452a275371e395fe96a4e690a54fb89c9bd155
Juan Carlos Martinez Burr <jcmb2049@gmail.com>
2021-11-01 17:44:56 -0500

    agregar componenete de card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'components/Card';
 
 
 function App() {
@@ -8,19 +9,12 @@ function App() {
     <div className="App">
       <span className='fondoNegro'>Hola mundo</span>
       <button type="button" className="btn btn-primary">Boton Inicio</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <div class="alert alert-primary" role="alert">
+      <button type="button" className="btn btn-success">Success</button>
+      <div className="alert alert-primary" role="alert">
       Esto es uan alerta!!!
 </div>
       
-      <div class="card" >
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+<Card/>
     </div>
   );
 }
