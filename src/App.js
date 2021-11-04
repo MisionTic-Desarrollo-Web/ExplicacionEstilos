4015be9cdd4ae3bb3f534eaab10411e4f9c790be
Juan Carlos Martinez Burr <jcmb2049@gmail.com>
2021-11-04 14:52:21 -0500

    agrgar explicacion sobre estilos con tailwind

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'components/Card';
+import './App.css';
+//import Card from 'components/Card';
 
 
 function App() {
   return (
     <div className="App">
-      <span className='fondoNegro'>Hola mundo</span>
-      <button type="button" className="btn btn-primary">Boton Inicio</button>
-      <button type="button" className="btn btn-success">Success</button>
-      <div className="alert alert-primary" role="alert">
-      Esto es uan alerta!!!
-</div>
-      
-<Card/>
+      <div className='flex flex-col md:flex-row'>
+        <div className='mx-2 bg-red-400'>Elemento 1</div>
+        <div className='mx-2 bg-green-400'>Elemento 2</div>
+        <div className='mx-2 bg-gray-500'>Elemento 3</div>
+        <div className='mx-2 bg-blue-300'>Elemento 4</div>
+        <button className='boton'>Haz Clic Aqui</button>
+        <button className='boton'>
+          Este es el boton 2
+        </button>
+      </div>          
     </div>
   );
 }
